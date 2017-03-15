4fc9d11953afa841d39ba749ffad38ff9102381d
Carine <hammescarine@gmail.com>
2017-03-14 23:31:46 -0300

    Start dependencies

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Content from './Content.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+		<Header title={"Bem vindo ao book-search"}/>
+		<Content title={"VAmos pesquisar"}/>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+export const Header = (props) =>
+	<div className="App-header">
+	  <h2>{props.title}</h2>
+	</div>;
