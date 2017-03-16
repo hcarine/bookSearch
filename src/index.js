0c81443279c15c8d44f46a41b15e26c0906d084b
Carine <hammescarine@gmail.com>
2017-03-16 20:33:26 -0300

    init router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
-import App from './App';
+import Router from './Router'
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router />,
   document.getElementById('root')
 );
