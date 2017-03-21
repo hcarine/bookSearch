36a4aaa7224d6c203544bd11c41264d9ef17a1cf
Carine <hammescarine@gmail.com>
2017-03-21 00:06:29 -0300

    star of pagination

--- src/Content.js
+++ src/Content.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardText, CardTitle, CardActions, Grid, Cell } from 'react-mdl';
 import {Link} from 'react-router-dom'
 import axios from 'axios'
+import {Infinite} from 'react-infinite'
 
 class Content extends Component {
 	
@@ -9,10 +10,10 @@ class Content extends Component {
 		
 		super(props)
 		this.state = {
-		  posts: []
+			posts: [],
+			elementsLenght: 0
 		}
-		this.refreshList= this.refreshList.bind(this)
-  }
+	}
 	
 	refreshList(query){
 		axios.get(`https://www.googleapis.com/books/v1/volumes?q=intitle:´${query}´&maxResults=20`)
@@ -23,6 +24,47 @@ class Content extends Component {
 			}
 		  });
 	}
+	getInitialState() {
+        return {
+            elements: this.buildElements(0),
+            isInfiniteLoading: false
+        }
+    }
+
+    buildElements(start, query ) {
+        var elements = [];
+       	axios.get(`https://www.googleapis.com/books/v1/volumes?q=intitle:´${query}´&startIndex´${start}´&maxResults=20`)
+		  .then(res => {
+			if( res.status === 200 && res.data.totalItems > 0){
+				const posts = res.data.items;
+				this.setState({ posts });
+				elements = elements + 20
+				return elements
+			}
+		  });
+        return elements;
+    }
+
+    handleInfiniteLoad() {
+        var that = this;
+        this.setState({
+            isInfiniteLoading: true
+        })
+        setTimeout(function() {
+            var elemLength = that.state.posts.length,
+                newElements = that.buildElements(elements);
+            that.setState({
+                isInfiniteLoading: false,
+                elements: that.state.elements.concat(newElements)
+            });
+        }, 2500);
+    }
+
+    elementInfiniteLoad() {
+        return <div className="infinite-list-item">
+            Loading...
+        </div>;
+    }
 
 	
 	render() {
@@ -33,21 +75,28 @@ class Content extends Component {
 				
 
 				<Grid className="demo-grid-1">
-					{this.state.posts.map(post =>
-						<Cell col={4} key={post.id}>	 
-							<Card shadow={0} style={{width: '320px', height: '320px', margin: '0', display:'flex'}}>
-								<CardTitle expand style={{color: '#fff', backgroundColor: '#46B6AC', textTransform: 'lowercase'}}>				
-									{post.volumeInfo.title}
-								</CardTitle>
-								<CardText>
-									{post.volumeInfo.title}
-								</CardText>
-								<CardActions border>
-									<Link to={"/book/"+post.id}>+ detalhes</Link>
-								</CardActions>
-							</Card>
-						</Cell>
-					)}
+					<Infinite 
+                         onInfiniteLoad={this.handleInfiniteLoad}
+                         loadingSpinnerDelegate={this.elementInfiniteLoad()}
+                         isInfiniteLoading={this.state.isInfiniteLoading}
+                         >
+						{this.state.posts.map(post =>
+									<Cell col={4} key={post.id}>	 
+										<Card shadow={0} style={{width: '320px', height: '320px', margin: '0', display:'flex'}}>
+											<CardTitle expand style={{color: '#fff', backgroundColor: '#46B6AC', textTransform: 'lowercase'}}>				
+												{post.volumeInfo.title}
+											</CardTitle>
+											<CardText>
+												{post.volumeInfo.title}
+											</CardText>
+											<CardActions border>
+												<Link to={"/book/"+post.id}>+ detalhes</Link>
+											</CardActions>
+										</Card>
+									</Cell>
+								)}
+        			</Infinite>
+					
 				</Grid>			 
 			</div>
 		)
